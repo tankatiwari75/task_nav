2f19f7d4c1586ba3cefb91fb756ab14a5f0b8c53
tiwaritanka75 <tiwaritanka75@gmail.com>
2023-10-20 11:39:33 -0400

    Developing MessageBox Page

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
+
 export const Chat = () => {
   return (
-    <div>Chat</div>
+    <>
+    <header className="flex bg-violet-600 h-16 ">
+        <span className="text-zinc-50 mt-4 font-bold text-lg ">Jasmina</span>
+    </header> 
+    <div>Message  Area</div>
+    <div  className='absolute bg-gray-200 bottom-0 w-full p-8 shadow-lg'>
+      <form className='containerWrap flex '>
+        <input className="input w-full focus:outline-none bg-gray-100 rounded-r-none " type="text"/>
+        <button className='w-auto h-10 bg-gray-800 text-sm text-white rounded-r-lg px-5 '>Send</button>
+      </form>
+    </div>
+    
+    </>
   )
 }
 export default Chat;
--- src/containers/MessagePage.js
+++ src/containers/MessagePage.js
@@ -9,11 +9,11 @@ export default function MessagePage() {
   return (
     <>
       <NavBar />
-      <div className="grid grid-cols-4 gap-4" style={{height:'75vh'}}>
-        <div className="bg-violet-600 m-4 rounded-xl overflow-hidden">
+      <div className="grid grid-cols-4 gap-2" style={{height:'75vh'}}>
+        <div className="bg-violet-600 m-4 mr-2 rounded-xl overflow-hidden">
           <SideBar />
         </div>
-        <div className="col-span-3">
+        <div className=" relative col-span-3 mt-4 mr-4 mb-4 border-2 rounded-xl overflow-hidden">
           <Chat />
         </div>
       </div>
